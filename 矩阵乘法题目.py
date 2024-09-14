9cd10ae2608dfe9332b510ffcfe1351316c46c71
Nicholas022400701 <116372750+Nicholas022400701@users.noreply.github.com>
2024-09-14 17:23:39 +0800

    Add files via upload

---
+++ 矩阵乘法题目.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def matrix_multiplication(x, y):
+    #在此处输入算法
+
+
+# In[ ]:
+
+
+assert matrix_multiplication([[2]], [[5]]) == [[10]]
+
+
+# In[ ]:
+
+
+assert matrix_multiplication([[1, 3]], [[5], [2]]) == [[11]]
+
+
+# In[ ]:
+
+
+assert matrix_multiplication([[3, 5], [2, 4]], [[1, 2, 5], [4, 3, 2]]) == [[23, 21, 25], [18, 16, 18]]
+
